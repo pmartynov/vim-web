9e9c9eeeeededeb2c20dc305228b2d371840eafc
dmitryorelopt <dmitryorelopt@gmail.com>
2018-07-30 18:05:50 +0300

    hide modal

--- src/components/commons/InfoModal/InfoModal.js
+++ src/components/commons/InfoModal/InfoModal.js
@@ -1,25 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import './infoModal.css';
+import ShowIf from '../../utils/ShowIf';
 
 class InfoModal extends Component {
 
 	render() {
 		return (
-			<div className="container_info-modal">
+			<ShowIf show={this.props.show} className="container_info-modal">
 				<div className="header_info-modal">
 				</div>
 				<div className="body_info-modal">
 				</div>
 				<div className="footer_info-modal">
 				</div>
-			</div>
+			</ShowIf>
 		);
 	}
 }
 
 const mapStateToProps = (state) => {
-	return {};
+	return {
+		show: false
+	};
 };
 
 
